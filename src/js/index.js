c5bbaf6b95df04d7e1968ac27cadf138ef161b23
Crush007 <sanya.tumanow2011@yandex.ru>
2024-05-01 18:29:07 +0400

    best-seller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
+import {
+	Fancybox
+} from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 Fancybox.bind('[data-fancybox]', {
@@ -9,3 +11,23 @@ Fancybox.bind('[data-fancybox]', {
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.best-sellers__slider', {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 24,
+
+	pagination: {
+		el: ".pagination-bullets",
+		bulletActiveClass: "bullet-Active",
+		bulletClass: "bullet",
+		clickable: true,
+	},
+});
