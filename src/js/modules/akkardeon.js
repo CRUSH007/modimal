6d04b07833e6c84f20b144f36a5f22e67e611b65
CRUSH007 <sanya.tumanow2011@yandex.ru>
2024-05-09 12:37:47 +0400

    publish

---
+++ src/js/modules/akkardeon.js
@@ -0,0 +1,17 @@
+function akkordion() {
+    const btns = document.querySelectorAll(".mobile-links__parent-btn");
+    btns.forEach(function(btn) {
+        btn.addEventListener("click", function() {
+            const content = btn.nextElementSibling;
+            const isOpen = btn.classList.toggle('active');
+
+            if(isOpen){
+                content.style.maxHeight = content.scrollHeight + 'px';
+            }else{
+                content.style.maxHeight = '0px';
+            }
+        });
+    });
+}
+
+export default akkordion;
